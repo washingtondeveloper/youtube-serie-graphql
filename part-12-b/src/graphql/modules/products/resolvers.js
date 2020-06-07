4827e853defdfb89913c8ebbbb48934610e24bdd
washingtondeveloper <washingtonprogramador@gmail.com>
2020-06-07 13:53:15 -0300

    remove part-12-b

--- part-12-b/src/graphql/modules/products/resolvers.js
+++
@@ -1,8 +0,0 @@
-const db = require("../../../db");
-
-module.exports = {
-  Query: {
-    products: () => db.products,
-    product: (_, { id }) => db.products.find((p) => p.id === id),
-  },
-};
--- part-12-b/src/graphql/modules/users/resolvers.js
+++
@@ -1,7 +0,0 @@
-const db = require("../../../db");
-
-module.exports = {
-  Query: {
-    users: () => db.users,
-  },
-};
