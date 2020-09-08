1215d1e4dc9663c236a5bfcf8a5c1d4d824fed3e
washingtondeveloper <washingtonprogramador@gmail.com>
2020-09-08 20:08:28 -0300

    part-27 add lib apollo-client

--- 27-graphql/src/App.js
+++ 27-graphql/src/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Form from "./components/Form";
 import Contatos from "./components/Contatos";
 import Header from "./components/Header";
 
+import { client } from "./config/client-graphql";
+import { gql } from "@apollo/client";
+
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    initial();
+  }, []);
+
+  function initial() {
+    client
+      .query({
+        query: gql`
+          query {
+            contatos {
+              id
+              nome
+              email
+              telefone
+            }
+          }
+        `,
+      })
+      .then((res) => console.log(res));
+  }
+
   return (
     <div className="container">
       <Header text="Contatos" />
